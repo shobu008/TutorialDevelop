d4cd22861e866fc560470384e01345ddc45642ca
shobu008 <shobu.okamoto@gmail.com>
2023-03-23 22:44:00 +0900

    コミット

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,12 +4,14 @@ import java.util.Set; // 追加
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult; // 追加
+import org.springframework.validation.annotation.Validated; // 追加
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam; // 追加
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -39,14 +41,20 @@ public class UserController {
         return "user/register";
     }
 
+    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+    // ----- 変更ここまで -----
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
