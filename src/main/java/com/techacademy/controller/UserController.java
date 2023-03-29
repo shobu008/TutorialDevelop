2e15e48064d901d66d98cc18bb14296929069a77
shobu008 <shobu.okamoto@gmail.com>
2023-03-29 23:02:43 +0900

    課題の提出

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -58,17 +58,25 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+        if(id != null) {
+            model.addAttribute("user", service.getUser(id));
+        }else {
+            model.addAttribute("user",user);
+        }
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@Validated User user,BindingResult res, Model model) {
         // User登録
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null,model,user);
+        }
+
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -7,15 +7,18 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne; // 追加
+import javax.persistence.PreRemove; // 追加
 import javax.persistence.Table;
 
-import javax.validation.constraints.Email; // 追加
-import javax.validation.constraints.Max; // 追加
-import javax.validation.constraints.Min; // 追加
-import javax.validation.constraints.NotEmpty; // 追加
-import javax.validation.constraints.NotNull; // 追加
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Length; // 追加
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import lombok.Data;
 
@@ -56,5 +59,20 @@ public class User {
     @Email // 追加
     @Length(max=50) // 追加
     private String email;
+
+    // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+    // ----- 追加ここまで -----
 
 }
